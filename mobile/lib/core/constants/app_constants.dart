b5151b88ce56edd0003a3b80ef62c37b0d9ae411
Orhun Kenger <orhunkenger1929@gmail.com>
2026-05-10 14:05:09 +0300

    fix: sunucu adresi ve eksik bağımlılıklar

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'http://YOUR_SERVER_IP:8000/api/v1';
+  static const String baseUrl = 'http://204.168.146.87/pixelwar/api/v1';
   static const int battleDurationSeconds = 30;
   static const int mapWidth = 200;
   static const int mapHeight = 200;
